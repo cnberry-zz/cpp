8641400ecaf07274d90f7a4a665b522376e62a20
Chris Berry <cnberry@gmail.com>
2018-01-21 16:06:16 -0800

    add slow recursive fib and fast dynamic programming fib

---
+++ fibonacci/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+/* Standard recursive fib program */
+int64_t fibonacci(int n) {
+	if (n <= 1 ) 
+		return 1;
+  
+	return ( fibonacci(n-1) + fibonacci(n-2) );
+
+}
+
+/* Dynamic programming style fib */
+void fast_fibonacci(int n) {
+	int64_t fib[100] = {1,1};
+
+	cout << "fst fib: " << fib[0] << " " << fib[1] << " ";
+	for (int i=2; i < n; i++) {
+		fib[i] = fib[i-2] + fib[i-1];
+		cout << fib[i] << " ";
+	}
+	cout << endl;
+
+}
+
+int main (int argc, char **argv) {
+          
+        /* get n from cmd line */
+        int n = atoi(argv[1]);       
+
+		fast_fibonacci(n);
+
+		cout << "std fib: ";
+        for(int i=0; i<n; i++) {
+           cout << fibonacci(i) << " ";
+        }
+		cout << endl;
+
+
+
+}
+
+
